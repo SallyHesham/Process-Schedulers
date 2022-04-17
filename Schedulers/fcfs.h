dced11b5223882d2bb11b5c597efd1b63c522372
Ahmed Ashraf <67614157+A357-a@users.noreply.github.com>
2022-04-17 21:47:52 +0200

    Create fcfs.h

---
+++ Schedulers/fcfs.h
@@ -0,0 +1,21 @@
+#include "process.h"
+#include "node.h"
+#include "list.h"
+#include <string>
+using namespace std;
+
+class fcfs {
+private:
+    int size;
+    Process *process  ;
+   
+   
+public:
+    
+    fcfs (Process processes [] , int n);
+    list gantt_chart();
+    double waiting_time ();
+    double turnarround_time();
+    };
+
+#endif // FCFS_H
