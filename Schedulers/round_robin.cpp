8749cc34988fae6ccb3d5180376bc9107b769953
salwa733 <85217207+salwa733@users.noreply.github.com>
2022-04-19 00:32:50 +0200

    update

--- Schedulers/round_robin.cpp
+++ Schedulers/round_robin.cpp
@@ -3,20 +3,23 @@
 round_robin::round_robin(Process* processes[], int n, int quan)
 {
     process = processes;
-    size = n-1;
+    size = n - 1;
     q = quan;
 }
 
 
 LinkedList round_robin::gantt_chart()
 {
-    LinkedList gantt; // need empty constructor
+    LinkedList gantt;
     int count = 0;
     int done = 0;
 
-    while (done < size) {
-        for (int i = 0; i < size; i++) {
-            if (process[i]->get_time_left() - q > 0) {
+    while (done <= size) {
+        for (int i = 0; i <= size; i++) {
+            if (process[i]->get_time_left() == 0)
+                continue;
+
+            else if (process[i]->get_time_left() - q > 0) {
                 count += q;
                 gantt.add_node(process[i]->get_name(), count - q, count);
                 process[i]->set_time_left(process[i]->get_time_left() - q);
@@ -26,12 +29,12 @@ LinkedList round_robin::gantt_chart()
                 count += process[i]->get_time_left();
                 gantt.add_node(process[i]->get_name(), count - process[i]->get_time_left(), count);
                 process[i]->set_time_left(0);
-                process[i]->set_end_time(count); // please add set_end_time and get_end_time
+                process[i]->set_end_time(count);
                 done++;
             }
         }
     }
-    
+
     return gantt;
 }
 
@@ -39,10 +42,10 @@ LinkedList round_robin::gantt_chart()
 double round_robin::waiting_time()
 {
     double waiting = 0.0;
-    for (int i = 0; i < size; i++) {
-        waiting += process[i]->get_end_time() - process[i]->get_burst(); // needs get_end_time
+    for (int i = 0; i <= size; i++) {
+        waiting += process[i]->get_end_time() - process[i]->get_burst() - process[i]->get_entry();
     }
-    return (waiting / size);
+    return (waiting / (size + 1));
 }
 
 
@@ -50,9 +53,9 @@ double round_robin::waiting_time()
 double round_robin::turnarround_time()
 {
     double turnarround = 0.0;
-    for (int i = 0; i < size; i++) {
-        turnarround += process[i]->get_end_time();
+    for (int i = 0; i <= size; i++) {
+        turnarround += process[i]->get_end_time() - process[i]->get_entry();
     }
-    return (turnarround / size);
+    return (turnarround / (size + 1));
 }
 
